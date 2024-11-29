f05cb94a3218262c94bc9cc80d5a51b024a0bf44
ntandomagana <ntandomagana@gmail.com>
2024-11-29 16:16:52 +0200

    fix imports issue

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- Frontend/src/app/nav/nav.component.ts
+++ Frontend/src/app/nav/nav.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RegisterComponent } from '../register/register.component';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  standalone: true,
+  imports: [RegisterComponent, LoginComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
