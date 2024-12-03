5744ed940c77e8fa4defc13c18933992a12e7ee4
ntandomagana <ntandomagana@gmail.com>
2024-12-03 15:54:45 +0200

    fix the roterlink issue on login page

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { RegisterComponent } from './register/register.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterLink, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
